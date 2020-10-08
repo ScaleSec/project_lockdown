55fba5a7b69e9533d694c0fc475a4a90fd9382d2
jdyke <jasonadyke@gmail.com>
2020-10-08 16:17:21 -0400

    logging and other logic updares

--- src/public_gcs_bucket/main.py
+++ src/public_gcs_bucket/main.py
@@ -1,32 +1,46 @@
-import logging
 import datetime
 import pytz
-from google.cloud import storage
 import base64
 import json
+import logging
 
-logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.ERROR)
+from google.cloud import storage
+import google.cloud.logging
 
 def pubsub_trigger(data, context):
     """
     Used with Pub/Sub trigger method to evaluate bucket for public access and remediate if public access exists
     """
-    logging.info("Received log containing the storage.setIamPermissions using Pub/Sub")
 
+    # Integrates cloud logging handler to python logging
+    create_logger()
+
+    logging.info("Received GCS public remediation log from Pub/Sub.")
+
+    # Converting log to json
     data_buffer = base64.b64decode(data['data'])
     log_entry = json.loads(data_buffer)
 
+    # Setting GCS bucket name and project ID variables from log
     bucket_name = log_entry['resource']['labels']['bucket_name']
     project_id = log_entry['resource']['labels']['project_id']
-
+    
+    # Configuring storage client
     storage_client = storage.Client()
+
     bucket = storage_client.bucket(bucket_name)
     policy  = bucket.get_iam_policy()
-    members_to_remove = eval_bucket(project_id,bucket_name, policy)
+
+    # Evaluating GCS bucket policy for public bindings
+    members_to_remove = eval_bucket(project_id, bucket_name, policy)
     
-    if bool(members_to_remove) != False:
-        remediated_policy = remove_public_iam_members_from_policy(bucket_name,members_to_remove)
+    if members_to_remove:
+        # Removes the public IAM bindings from the bucket and returns the new bucket policy
+        remediated_policy = remove_public_iam_members_from_policy(bucket_name, members_to_remove)
+
+        # Sets the new bucket policy
         bucket.set_iam_policy(remediated_policy)
+
         logging.info('Finished updating {} IAM Policy'.format(bucket_name))
     else:
         logging.info('No Members to remove from {}'.format(bucket_name))
@@ -38,15 +52,18 @@ def eval_bucket(project_id, bucket_name, policy):
     members_to_remove = {}
     for role in policy:
         members = policy[role]
-        print('Role: {}, Members: {}'.format(role, members))
-        for x in members:
-            if  x == "allAuthenticatedUsers" or x == "allUsers":
-                members_to_remove.update({role : x})
-                logging.info('Removed {} with role {} from {}.'.format(x, role, bucket_name))
-            else:
-                logging.info("Member {} in role {} not a public members in {}".format(x, role,bucket_name))
+        text = 'Role: {}, Members: {}'.format(role, members)
+        logging.info(text)
 
-    logging.info("Members to remove {}".format(members_to_remove))
+        for x in members:
+            if x == "allAuthenticatedUsers" or x == "allUsers":
+                # Add member to list only if public
+                members_to_remove.update({role : x})
+                logging.info('Found {} with role {} on {}.'.format(x, role, bucket_name))
+            else:
+                logging.info("Member {} with role {} is not public.".format(x, role))
+
+    logging.info("Total list of public IAM bindings to remove: {}".format(members_to_remove))
 
     return members_to_remove
 
@@ -54,11 +71,28 @@ def remove_public_iam_members_from_policy(bucket_name, members_to_remove):
     """
     Takes a dictionary of roles and members to remove and removes them from an IAM Policy.  Returns IAM Policy without public memebers
     """
+
+    # Create storage client
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     policy = bucket.get_iam_policy()
+
+    # Remove public members from GCS bucket policy
     for role in members_to_remove:
         policy[role].discard(members_to_remove[role])
         logging.info('Removed member {} with role {} from {}.'.format(members_to_remove[role], role, bucket_name))
 
+    # Return updated private bucket policy
     return policy
+
+
+def create_logger():
+    
+    # Instantiates a cloud logging client
+    client = google.cloud.logging.Client()
+
+    # Retrieves a Cloud Logging handler based on the environment
+    # you're running in and integrates the handler with the
+    # Python logging module
+    client.get_default_handler()
+    client.setup_logging()
