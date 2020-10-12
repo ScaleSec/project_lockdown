6767b2afcbd79d0debd90a23cb9444dc9b5ecd1d
jdyke <jasonadyke@gmail.com>
2020-10-12 10:22:38 -0400

    more efficient log sink query

--- modules/public_gcs_bucket/variables.tf
+++ modules/public_gcs_bucket/variables.tf
@@ -19,7 +19,7 @@ variable "name" {
 
 variable "org_sink_filter" {
   description = "The log filter to apply to the Org Level Cloud Logging export."
-  default     = "resource.type=\"gcs_bucket\" protoPayload.methodName=\"storage.setIamPermissions\""
+  default     = "resource.type=\"gcs_bucket\" protoPayload.methodName=\"storage.setIamPermissions\" AND protoPayload.serviceData.policyDelta.bindingDeltas.member=\"allUsers\" OR protoPayload.serviceData.policyDelta.bindingDeltas.member=\"allAuthenticatedUsers\""
 }
 
 variable "function_perms" {
