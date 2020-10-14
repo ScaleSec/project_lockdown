f004c74190eba50feb080123db8bcdc5e7b60c8e
jdyke <jasonadyke@gmail.com>
2020-10-14 10:02:37 -0400

    comment updates

--- src/public_gcs_bucket/main.py
+++ src/public_gcs_bucket/main.py
@@ -47,13 +47,13 @@ def eval_bucket(project_id, bucket_name, policy, bucket):
         # Empty list to add public IAM bindings to
         member_bindings_to_remove = {}
 
-        # For each IAM binding, find the role and member
+        # For each IAM binding, find the members
         members = policy[role]
 
         # For every member, check if they are public
         for member in members:
             if member == "allAuthenticatedUsers" or member == "allUsers":
-                # Add member to list if member is public
+                # Add member and role to list if member is public
                 member_bindings_to_remove[member] = role
                 logging.info('Found public member: {} with role: {} on bucket: {}.'.format(member, role, bucket_name))
             else:
@@ -61,13 +61,12 @@ def eval_bucket(project_id, bucket_name, policy, bucket):
 
         if member_bindings_to_remove:
             logging.info('List of public IAM bindings to remove for role: {} - {}'.format(role, member_bindings_to_remove))
-            # Removes the public IAM bindings from the bucket for this specific role and returns the new bucket policy
+            # Removes the public IAM bindings from the bucket for this specific role
             remove_public_iam_members_from_policy(bucket_name, member_bindings_to_remove, bucket)
 
 def remove_public_iam_members_from_policy(bucket_name, member_bindings_to_remove, bucket):
     """
     Takes a dictionary of roles with public members and removes them from a GCS bucket Policy object.  
-    Returns the bucket Policy without public members.
     """
     # Re-checks the bucket policy to catch previously made updates
     # This is critical in the event that a user makes a `setIamPermissions` API call
@@ -78,7 +77,7 @@ def remove_public_iam_members_from_policy(bucket_name, member_bindings_to_remove
     for member in member_bindings_to_remove:
         policy[member_bindings_to_remove[member]].discard(member)
 
-    # Set the new private bucket policy
+    # Set the new bucket policy
     try:
         bucket.set_iam_policy(policy)
         logging.info('Finished updating the IAM permissions on bucket: {} for role: {}'.format(bucket_name, member_bindings_to_remove[member]))
